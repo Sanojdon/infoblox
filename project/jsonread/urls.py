eb262b9fcecbc80633ac885fca4adfdb7b25018b
sanojdon <sanoj.mv.93@gmail.com>
2020-09-09 18:31:30 +0530

    Added Authentication

--- project/jsonread/urls.py
+++ project/jsonread/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.conf.urls import include, re_path
+from rest_framework.authtoken.views import obtain_auth_token
 # from rest_framework.urlpatterns import format_suffix_patterns
 
 from jsonread import views
@@ -9,4 +10,5 @@ urlpatterns = [
     path('api/all', views.DataReadList.as_view()),
     path('api/<int:pk>', views.DataReadDetail.as_view()),
     path('api/new', views.DataReadCreate.as_view()),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- project/project/settings.py
+++ project/project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication', 
+    ],
 }
 
 
